34901f20921c081d2bdab352c7878db15220272f
ZiglaCity <ziglacity@gmail.com>
2025-02-10 23:02:31 +0000

    Updated solution for contest Problem 5

--- contest2/E_Equal_Sums.py
+++ contest2/E_Equal_Sums.py
@@ -1,27 +1,34 @@
-# k = int(input())
-# dic = {}
-# for i in range(k):
-#     # get each of the sequences
-#     length = int(input())
-#     arr = list(map(int, input().split()))
-#     dic[i + 1] = arr
-
-# for i in dic:
-#     for j in dic:
-#         if i != j:
-#             sum_i = sum(dic[i])
-#             sum_j = sum(dic[j])
-#             for element in dic[i]:
-#                 new_sum = sum_i - element
-#                 for e in dic[j]:
-#                     if new_sum == sum_j - e:
-#                         print(new_sum)
-#                         print(sum_j - e)
-#                         print("YES")
-#                         print(i, dic[i].index(element) + 1)
-#                         print(j, dic[j].index(e) + 1)
+def find_sequences(k, sequences):
+    sum_dict = {}  # Dictionary to store sum differences
+    
+    for idx in range(k):
+        sequence = sequences[idx]
+        total_sum = sum(sequence)
+        
+        for element_index, element in enumerate(sequence):
+            new_sum = total_sum - element
+            
+            if new_sum in sum_dict:
+                other_sequence_idx, other_element_index = sum_dict[new_sum]
                 
-# print("NO")
+                # Ensure different sequences are used
+                if other_sequence_idx != idx:
+                    print("YES")
+                    print(other_sequence_idx + 1, other_element_index + 1)
+                    print(idx + 1, element_index + 1)
+                    return
+                
+            # Store the sum and indices
+            sum_dict[new_sum] = (idx, element_index)
+    
+    print("NO")
 
-# pick two sequence such that;
-# we can remove one from i and one from j and their sums would be equal
+# Read input
+k = int(input())
+sequences = []
+for _ in range(k):
+    length = int(input())
+    arr = list(map(int, input().split()))
+    sequences.append(arr)
+
+find_sequences(k, sequences)
