ad6c00668b0bd04bba5bb643b3534ab1a541245c
ZiglaCity <ziglacity@gmail.com>
2025-08-03 18:56:59 +0000

    fix matrix comaprison

--- sample_algos/gauss_iteration.py
+++ sample_algos/gauss_iteration.py
@@ -65,7 +65,7 @@ def verify_final_X(X : list, A : list[list], d: list) -> list:
 
 def compare_the_Ds(initial_d : list, final_d : list) -> bool:
     for i in range(len(initial_d)):
-        if int(initial_d[i]) != int(final_d[i]):
+        if round(initial_d[i]) != round(final_d[i]):
             print(f"WRONG {initial_d[i]} != {final_d[i]}")
             return False
     print(f"CORRECT {initial_d[i]} == {final_d[i]}")
